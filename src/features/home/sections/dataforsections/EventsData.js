d592797cb32382672430b73ac081bf6d97ac8d15
Ashmilll <ashmilkurikkal12@gmail.com>
2026-01-08 21:06:51 +0530

    changed event tag

--- src/features/home/sections/dataforsections/EventsData.js
+++ src/features/home/sections/dataforsections/EventsData.js
@@ -1,13 +1,13 @@
 const eventsData = [
   {
     id: 1,
-    title: "3D Modelling competition",
+    title: "Modeling Competition",
     date: "Oct 25-27, 2024",
     location: "Main Seminar Hall, North Block, FISAT",
     shortDesc: "A competition to test your 3D Modelling skills.",
     fullDesc: "",
     image: "https://images.unsplash.com/photo-1551818255-e6e10975bc17?auto=format&fit=crop&w=800&q=80",
-    tags: ["Conference", "Hybrid"]
+    tags: ["Competition", "3D"]
   }
 ];
 
